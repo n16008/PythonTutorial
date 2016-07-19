0bc5c13f7164cc363d34d3d1676f0615b7f632b0
n16008 <n16008@std.it-college.ac.jp>
2016-07-19 12:20:13 +0900

    Initial commit

---
+++ fibo.py
@@ -0,0 +1,19 @@
+# フィボナッチ数モジュール
+
+
+def fib(n):  # nまでのフィボナッチ級数を書き出す
+
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a + b
+        print()
+
+
+def fib2(n):  # nまでのフィボナッチ級数を返す
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a + b
+    return result
